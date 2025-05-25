5058838b9f0a60927b5ffceb98f9e6257e285ea6
Nipun Dilshan <harshakaldera540@gmail.com>
2025-05-25 09:10:39 +0530

    Add user stream implementation and update dependencies

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,15 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repository/src/entities/entities.dart';
 import 'package:user_repository/src/models/user.dart';
 import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepo implements UserRepository {
   final FirebaseAuth _firebaseAuth;
-  final userCollection = FirebaseFirestore.instance.collection('users');
+  final usersCollection = FirebaseFirestore.instance.collection('users');
 
   FirebaseUserRepo({
     FirebaseAuth? firebaseAuth,
   }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Stream<MyUser?> get user {
+    return _firebaseAuth.authStateChanges().flatMap((firebaseUser) async* {
+      if (firebaseUser == null) {
+        yield MyUser.empty;
+      } else {
+        yield await usersCollection.doc(firebaseUser.uid).get().then((value) =>
+            MyUser.frontEntity(MyUserEntity.fromDocument(value.data()!)));
+      }
+    });
+  }
 
   @override
   Future<void> logout() {
@@ -34,8 +48,4 @@ class FirebaseUserRepo implements UserRepository {
     // TODO: implement signUp
     throw UnimplementedError();
   }
-
-  @override
-  // TODO: implement user
-  Stream<MyUser?> get user => throw UnimplementedError();
 }
