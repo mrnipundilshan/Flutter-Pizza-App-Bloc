583be2e4a4d7e9ce008620bd0e351ad577ddb52c
Nipun Dilshan <harshakaldera540@gmail.com>
2025-05-25 10:29:33 +0530

    Implement RepositoryProvider for AuthenticationBloc in MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider<AuthenticationBloc>(
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: MyAppView(),
+    );
   }
 }
