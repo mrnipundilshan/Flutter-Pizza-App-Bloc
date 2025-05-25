0f55693e7345a0b9e57c3db92c360261a93e8b26
Nipun Dilshan <gnsrupasinghe@students.nsbm.ac.lk>
2025-05-25 19:58:55 +0530

    Add MaterialApp configuration to MyAppView for improved app structure

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -5,6 +5,17 @@ class MyAppView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Pizza Delivery",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+    );
   }
 }
