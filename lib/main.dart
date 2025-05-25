d741d60c6541e8e2bf9118d7909d772bee7cbdc2
Nipun Dilshan <gnsrupasinghe@students.nsbm.ac.lk>
2025-05-25 19:14:06 +0530

    Add Firebase configuration and initialize Firebase in the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_app_bloc/app.dart';
+import 'package:pizza_app_bloc/firebase_options.dart';
 import 'package:pizza_app_bloc/simple_bloc_observer.dart';
 import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
 }
