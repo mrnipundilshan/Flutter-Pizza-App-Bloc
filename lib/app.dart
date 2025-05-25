11190dfe271cb760d18e35e427c633ce46b81e0f
Nipun Dilshan <harshakaldera540@gmail.com>
2025-05-25 10:21:51 +0530

    Update NDK version and modify MyApp constructor to accept UserRepository

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final UserRepository userRepository;
+  const MyApp(this.userRepository, {super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_app_bloc/app.dart';
 import 'package:pizza_app_bloc/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MyApp());
+  runApp(MyApp(FirebaseUserRepo()));
 }
