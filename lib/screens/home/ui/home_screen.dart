c5d793f317b8a7e4bf87f05403e5af43ee6d108b
Nipun Dilshan <gnsrupasinghe@students.nsbm.ac.lk>
2025-05-27 08:05:56 +0530

    Enhance HomeScreen with SignInBloc integration and update UI elements; add asset declaration in pubspec.yaml

--- lib/screens/home/ui/home_screen.dart
+++ lib/screens/home/ui/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app_bloc/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,9 +12,40 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(
-          "Pizza",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        title: Row(
+          children: [
+            Image.asset("assets/8.png", scale: 14),
+            SizedBox(width: 5),
+            Text(
+              "Pizza",
+              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+            ),
+          ],
+        ),
+
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.cart)),
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignOutRequired());
+            },
+            icon: Icon(CupertinoIcons.arrow_right_to_line),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 9 / 16,
+          ),
+          itemCount: 4,
+          itemBuilder: (context, int i) {
+            return Container(color: Colors.red);
+          },
         ),
       ),
     );
