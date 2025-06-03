5258581a7fa42fc97fcf558aaa9c54ba3c915fe4
Nipun Dilshan <gnsrupasinghe@students.nsbm.ac.lk>
2025-06-03 19:54:10 +0530

    Add DetailsScreen layout with enhanced UI and MyMacroWidget integration; update pubspec.yaml for font_awesome_flutter dependency

---
+++ lib/components/macro.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyMacroWidget extends StatelessWidget {
+  final String title;
+  final int value;
+  final IconData icon;
+  const MyMacroWidget({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade400,
+              offset: Offset(2, 2),
+              blurRadius: 5,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            FaIcon(icon, color: Colors.redAccent),
+            SizedBox(height: 4),
+            Text(
+              title == "calories" ? '$value $title' : '${value}g $title',
+              style: TextStyle(fontSize: 10),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/ui/details_screen.dart
+++ lib/screens/home/ui/details_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pizza_app_bloc/components/macro.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -8,18 +11,145 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.background),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width - (40),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(30),
-              image: DecorationImage(image: AssetImage('assets/1.png')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width - (40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(3, 3),
+                    blurRadius: 5,
+                  ),
+                ],
+                image: DecorationImage(image: AssetImage('assets/1.png')),
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 30),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(3, 3),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            "Truffle Temptation Extravaganza",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "\$12.00",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                                Text(
+                                  "\$15.00",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey,
+                                    decoration: TextDecoration.lineThrough,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    Row(
+                      children: [
+                        MyMacroWidget(
+                          title: "calories",
+                          value: 256,
+                          icon: FontAwesomeIcons.fire,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: "Protien",
+                          value: 36,
+                          icon: FontAwesomeIcons.dumbbell,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: "Fat",
+                          value: 21,
+                          icon: FontAwesomeIcons.oilWell,
+                        ),
+                        SizedBox(width: 10),
+                        MyMacroWidget(
+                          title: "Carbs",
+                          value: 38,
+                          icon: FontAwesomeIcons.breadSlice,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          elevation: 3.0,
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Buy Now",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
